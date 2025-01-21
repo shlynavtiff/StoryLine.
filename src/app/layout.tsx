7dfd698231fa6a89250e3c992eeda2077ecbc521
rohyoonseo <ajnaval67@gmail.com>
2025-01-21 12:27:55 +0800

    sign in header added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import HeaderSignin from "../components/HeaderSignin";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header/>
+        <HeaderSignin/>
         {children}
         <Footer/>
       </body>
--- src/components/HeaderSignin.jsx
+++ src/components/HeaderSignin.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const HeaderSignin = () => {
     return (
-        <div>HeaderSignin</div>
+        <div className='flex flex-row gap-5 items-center justify-between w-full p-6'>
+            <div className='text-2xl '>
+                StoryLine.
+            </div>
+            <div className='flex flex-row  items-center'>
+                <ul className='flex flex-row sm:gap-4 md:gap-8 text-sm items-center'>
+                    <li><a href="" className='hidden md:block'>About</a></li>
+                    <li><a href="" className='hidden sm:block'>Sign in</a></li>
+                    <li><a href="" className='hidden md:block'>Write</a></li>
+                    <button className='bg-[#242424] py-2 rounded-full cursor-pointer text-white px-4 border-[1px]border-[#414141]  hover:bg-[#414141]'>
+                        Get started
+                    </button>
+                </ul>
+            </div>
+        </div>
     )
 }
 
