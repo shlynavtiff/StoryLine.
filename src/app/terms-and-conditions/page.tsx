52cbb06e7dd1067739d80924a1d2a54ba29a0a05
rohyoonseo <ajnaval67@gmail.com>
2025-01-22 09:58:50 +0800

    terms and conditions page added

---
+++ src/app/terms-and-conditions/page.tsx
@@ -0,0 +1,208 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+
+            <div className='text-[2rem] font-semibold'>
+                Terms and Conditions
+            </div>
+
+            <div className='mt-5'>
+                <i>Last update:</i> 22 January, 2025
+            </div>
+
+            <div className='mt-5'>
+                Welcome to StoryLine. By accessing or using our platform, you agree
+                to comply with and be bound by the following terms and conditions ("Terms"). If you
+                do not agree to these Terms, please do not use the platform.
+            </div>
+
+            <div className='mt-5'>
+                <h2 className='text-3xl font-semibold'>1. Acceptance of Terms</h2>
+                <div className='mt-2'>
+                    By creating an account, accessing, or using the platform, you accept these Terms
+                    and our Privacy Policy. These Terms apply to all users, including bloggers, readers,
+                    and other contributors.
+                </div>
+                <div className='mt-2'>
+                    These Terms constitute the entire agreement between you and StoryLine.
+                    concerning your use of the platform. Any additional or different terms proposed by you
+                    are expressly rejected unless agreed to in writing by us.
+                </div>
+            </div>
+
+            <div className='mt-5'>
+                <h2 className='text-3xl font-semibold'>2. User Accounts</h2>
+                <div className=''>
+                    <ul className='list-disc pl-5 flex flex-col gap-2 mt-5'>
+                        <li>
+                            <strong>Eligibility:</strong> You must be at least 13 years old to create an account.
+                            If you are under 18, parental or guardian consent is required.
+                        </li>
+                        <li>
+                            <strong>Account Security:</strong> You are responsible for maintaining the
+                            confidentiality of your account credentials and for all activities that occur under
+                            your account.
+                        </li>
+                        <li>
+                            <strong>Account Termination:</strong> We reserve the right to suspend or terminate
+                            accounts for violations of these Terms or any applicable laws..
+                        </li>
+                        <li>
+                            <strong>Account Information:</strong> You agree to provide accurate and complete
+                            information when creating your account and to update it as necessary to ensure its
+                            accuracy.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className='mt-5'>
+                <h2 className='text-3xl font-semibold'>3. Content Ownership and Rights</h2>
+                <div className=''>
+                    <ul className='list-disc pl-5 flex flex-col gap-2 mt-5'>
+                        <li>
+                            <strong>Your Content: </strong>You retain ownership of the content you create and post.
+                            By posting on our platform, you grant us a non-exclusive, worldwide, royalty-free license
+                            to display, distribute, and promote your content.
+                        </li>
+                        <li>
+                            <strong>Prohibited Content: </strong>  You agree not to post content that is illegal,
+                            harmful, offensive, or infringes on the rights of others.
+                        </li>
+                        <li>
+                            <strong>Content Moderation:</strong> We reserve the right to remove or restrict
+                            access to content that violates these Terms or our policies.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className='mt-5'>
+                <h2 className='text-3xl font-semibold'>4. Platform Use</h2>
+                <div className=''>
+                    <ul className='list-disc flex flex-col gap-2 mt-5 pl-5'>
+                        <li> <strong>Permitted Use:</strong>The platform is for personal, non-commercial use unless explicitly authorized.</li>
+                        <li> <strong>Prohibited Activities:</strong>You agree not to:</li>
+                        <ul className='list-disc pl-5 flex flex-col gap-2 '>
+                            <li>
+                                Use the platform for illegal purposes.
+                            </li>
+                            <li>
+                                Violate any applicable laws or regulations.
+                            </li>
+                            <li>
+                                Attempt to hack, disrupt, or harm the platform.
+                            </li>
+                            <li>
+                                Misuse the platform to distribute malware, spam, or other malicious content.
+                            </li>
+                            <li>
+                                Use automated systems, such as bots, to interact with the platform without authorization.
+                            </li>
+                        </ul>
+                    </ul>
+                </div>
+            </div>
+
+            <div className='mt-5'>
+                <h2 className='text-3xl font-semibold'>5. Intellectual Property</h2>
+                <div className=''>
+                    <ul className='list-disc pl-5 flex flex-col gap-2 mt-5'>
+                        <li>
+                            The platform and its content (excluding user-generated content) are the property of StoryLine.
+                            or its licensors and are protected by copyright, trademark, and other intellectual property laws.
+                        </li>
+                        <li>
+                            Unauthorized use of the platform's intellectual property is strictly prohibited and may result in legal action.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className='mt-5'>
+                <h2 className='text-3xl font-semibold'>6. Third-Party Links</h2>
+                <div className=''>
+                    <ul className='list-disc pl-5 flex flex-col gap-2 mt-5'>
+                        <li>
+                            Our platform may contain links to third-party websites or services. We are not responsible for the content,
+                            policies, or practices of third-party sites. You access third-party sites at your own risk.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className='mt-5'>
+                <h2 className='text-3xl font-semibold'>7. Indemnification</h2>
+                <div className=''>
+                    <ul className='list-disc pl-5 flex flex-col gap-2 mt-5'>
+                        <li>
+                            You agree to indemnify and hold harmless StoryLine., its affiliates, and employees from any claims,
+                            liabilities, damages, or expenses arising from your use of the platform or violation of these Terms.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className='mt-5'>
+                <h2 className='text-3xl font-semibold'>8. Changes to Terms</h2>
+                <div className=''>
+                    <ul className='list-disc pl-5 flex flex-col gap-2 mt-5'>
+                        <li>
+                            We may update these Terms from time to time. Changes will be effective upon posting. Continued use of
+                            the platform signifies acceptance of the updated Terms.
+                        </li>
+                        <li>
+                            We will notify you of significant changes by posting a notice on the platform or emailing registered users.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className='mt-5'>
+                <h2 className='text-3xl font-semibold'>9. Termination of Use</h2>
+                <div className=''>
+                    <ul className='list-disc pl-5 flex flex-col gap-2 mt-5'>
+                        <li>
+                            We reserve the right to terminate or suspend access to the platform without prior notice for
+                            conduct that violates these Terms or is harmful to other users or the platform.
+                        </li>
+                    </ul>
+                </div>
+                <p className='mt-5'>
+                    If your account is terminated, you may lose access to your data and content. We are not liable for any loss of data resulting from account
+                    termination. You may request a copy of your data before your account is suspended or closed. Please contact our support team for assistance.
+                </p>
+            </div>
+
+            <div className='mt-5'>
+                <h2 className='text-3xl font-semibold'>10. Governing Law</h2>
+                <div className=''>
+                    <ul className=' flex flex-col gap-2 mt-5'>
+                        <li>
+                            These Terms are governed by and construed in accordance with applicable laws, including but not limited to the provisions of the Republic Act 10173,
+                            also known as the Data Privacy Act of 2012. By using Orgamix, you acknowledge and agree to comply with all relevant legal obligations and regulations
+                            associated with your use of the platform.
+                        </li>
+                        <li>
+                            You further agree that any disputes, claims, or controversies arising out of or relating to these Terms or your use of Orgamix will be resolved
+                            through appropriate legal channels, subject to the jurisdiction determined by governing laws.
+                        </li>
+                        <li>
+                            Should any provision of these Terms be deemed invalid, unenforceable, or contrary to applicable law, such provision will be severed, and the
+                            remainder of the Terms will continue to remain valid, binding, and enforceable to the fullest extent permitted by law.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className='my-12'>
+                By using StoryLine., you acknowledge that you have read, understood, and agreed to these Terms and Conditions. 
+            </div>
+
+        </div>
+    )
+}
+
+export default page
