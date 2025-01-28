5b14f9bd710dba01d04c380174e6ed19052a0efa
rohyoonseo <ajnaval67@gmail.com>
2025-01-28 14:34:44 +0800

    added email auth

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,8 +1,88 @@
-import React from 'react'
+'use client'
+
+import React, { FormEvent, useEffect, useState } from 'react'
+import { supabase } from '../utils/supabaseClient'
+import { useRouter } from 'next/navigation'
 
 const page = () => {
+  const [email, setEmail] = React.useState('')  
+  const [password, setPassword] = React.useState('')
+  const [loading, setLoading] = useState(false)
+  const router = useRouter()
+
+
+  async function signIn(e: FormEvent) {
+    console.log("signing in")
+    e.preventDefault()
+
+
+    setLoading(true)
+    if (loading) {
+        return
+    }
+
+    console.log("signing in")
+    if (!email || !password) {
+        setLoading(false)
+        return alert("Please type something!")
+
+    }
+
+    try {
+        const { data: session, error: signInError }: any = await supabase.auth.signInWithPassword({
+            email,
+            password,
+        });
+
+        if (signInError) throw signInError;
+        const user = session?.user;
+
+        if (user?.email_confirmed_at === null) {
+            setLoading(false)
+            return alert("Please verify your email first!")
+        } else {
+
+            setLoading(false)
+          router.push('/')
+
+        }
+
+    }
+    catch (err: any) {
+        console.log(err)
+        if (err.status === 400) {
+            alert('Invalid login credentials')
+        } else if (err.status === 404) {
+            alert('User not found')
+        } else {
+            alert('An unexpected error occurred')
+        }
+        setLoading(false)
+    }
+}
+
+
+    
+
+  
+  
   return (
-    <div>page</div>
+    <div>
+      <h1>Sign In</h1>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={signIn}>Sign In</button>
+    </div>
   )
 }
 
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,8 +1,134 @@
-import React from 'react'
+'use client'
 
+import React, { FormEvent, useState } from 'react'
+import { supabase } from '../utils/supabaseClient'
 const page = () => {
+  
+  const [email, setEmail] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+  const [repPassword, setRepPassword] = useState<string>("")
+  const [Username, setUsername] = useState<string>("")
+
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+
+  async function createUser(params: string) {
+    try {
+        const { error } = await supabase.from('accounts').insert({
+            userid: params,
+            username: Username,
+            password: password,
+            email: email,
+        })
+        if (error) {
+            console.error('Error inserting data:', error);
+        } else {
+            setUsername("")
+            setEmail("")
+            setRepPassword("")
+            setPassword("")
+            setIsLoading(false)
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+function createUserAccount(e: FormEvent) {
+  e.preventDefault();
+
+
+  if (!Username || !email || !password || !repPassword) {
+      return console.log("Please fill up all inputs!");
+  }
+
+  if (password !== repPassword) {
+      return console.log("Password fields do not match!");
+  }
+
+  if (password.length < 7) {
+      return alert("Please make your password stronger!");
+  }
+
+  if (isLoading) {
+      return; // Prevent duplicate requests
+  }
+
+  setIsLoading(true);
+
+
+  // Sign up user with Supabase
+  supabase.auth
+      .signUp({
+          email: email,
+          password: password,
+      })
+      .then((response) => {
+          const user = response as any;
+          setIsLoading(false);
+
+          if (user) {
+              createUser(user.data.user.id);
+              console.log("User signed up successfully:", user);
+              
+          }
+      })
+      .catch((error: { message: string | string[] }) => {
+          setIsLoading(false);
+
+          if (error) {
+              console.log(error);
+
+              if (error.message.includes('For security purposes, you can only request')) {
+                  return alert("For security purposes, you can only request a new verification email every 5 minutes.");
+              }
+              if (error.message.includes("already registered") || error.message.includes("email already in use")) {
+                  return alert("Email is already in use!");
+              }
+
+              console.error("Signup error:", error.message);
+              return alert("An error occurred. Please try again.");
+          }
+
+          alert("Something went wrong. Please try again later.");
+      });
+}
+
+
+
+  
   return (
-    <div>page</div>
+    <form onSubmit={createUserAccount}>
+      <h1>Sign Up</h1>
+      <input
+        type="text"
+        placeholder="Username"
+        value={Username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Repeat Password"
+        value={repPassword}
+        onChange={(e) => setRepPassword(e.target.value)}
+      />
+      <button>
+        {isLoading ? "loading.."  : "Sign Up"}
+      </button>
+
+      </form>
   )
 }
 
