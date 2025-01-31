7bf148ab23fb4788ceff38390f7ea19487b6777b
rohyoonseo <ajnaval67@gmail.com>
2025-01-31 20:01:45 +0800

    routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import LandingPage from './assets/pages/LandingPage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    
+    <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+    </Router>
   )
 }
 
